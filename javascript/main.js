625ec38ffe930883ccb335f32da80399ecef647e
KayT98 <khanhtruong109@yahoo.com>
2023-08-21 12:28:45 -0500

    added folders and changed files to correct paths

--- javascript/main.js
+++ javascript/main.js
@@ -3,13 +3,12 @@ function error() {
     let username = document.getElementById("username").value
     let email = document.getElementById("email").value
     if  (username == "") {
-        alert("Enter your name bitch")
+        alert("Enter your name")
     }
     else if (email == "") {
-        alert("Enter your email as well bitch, remember to add the damn message too :|")
+        alert("Enter your email as well, remember to add the message too")
     }
     else {
         alert("You are good to go :)")
     }
-    return false;
 }
